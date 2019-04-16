bfa12271ea81f02bf770df42d941a7c7882db207
Mateusz Jamrowski <kermaxpl@gmail.com>
2019-04-16 23:07:36 +0200

    Unit test for scaler

--- Tests/test_scaler.py
+++ Tests/test_scaler.py
@@ -67,6 +67,81 @@ class TestScaler(TestCase):
             Scaler(calc_std=False, calc_mean=True, axis=1).transform(np.array([[0, 5, 10]])).tolist(),
             [[-5, 0, 5]])
 
-        # self.assertAlmostEqual(
-        #     Scaler(calc_std=False, calc_mean=True, axis=1).transform(np.array([[0], [5], [10]])).tolist(),
-        #     [[-5], [0], [5]])
+        input = Scaler(calc_std=False, calc_mean=True, axis=1).transform(np.array([[0], [5], [10]]))
+        result = [[0], [0], [0]]
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+        self.assertAlmostEqual(
+            Scaler(calc_std=False, calc_mean=True, axis=1).transform(np.array([[1, 0, 3.5]])).tolist(),
+            [[-.5, -1.5, 2]])
+
+        self.assertAlmostEqual(Scaler(calc_std=False, calc_mean=True).transform(np.array([[1, 0, 3.5]])).tolist(),
+                               [[0, 0, 0]])
+
+        input = Scaler(calc_std=False, calc_mean=True, axis=1).transform(np.array([[1], [0], [3.5]]))
+        result = [[0], [0], [0]]
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+        input = Scaler(calc_std=False, calc_mean=True, axis=0).transform(np.array([[1], [0], [3.5]]))
+        result = [[-.5], [-1.5], [2]]
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+    def test_transform_matrix_mean(self):
+        """
+        Test matrix with mean centering.
+        """
+        matrix = np.array([[21, 6, 9, 0],
+                           [9, 4, 1, 2],
+                           [6, 5, 8, 1]], dtype=np.float64)
+
+        input = Scaler(calc_mean=True, calc_std=False, axis=0).transform(matrix)
+
+        result = np.array([[9., 1., 3., -1.],
+                           [-3., -1., -5., 1.],
+                           [-6., 0., 2., 0.]])
+
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+        input = Scaler(calc_mean=True, calc_std=False, axis=1).transform(matrix)
+
+        result = np.array([[12., -3., 0., -9.],
+                           [5., 0., -3., -2.],
+                           [1., 0., 3., -4.]])
+
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+        matrix = np.array([[0.5, -0.9, 0.12],
+                           [7.1, 9.5, 2],
+                           [2.36, 1, 1]], dtype=np.float64)
+
+        input = Scaler(calc_mean=True, calc_std=False, axis=0).transform(matrix)
+
+        result = np.array([[-2.82, -4.1, -.92],
+                           [3.78, 6.3, 0.96],
+                           [-0.96, -2.2, -0.04]])
+
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+        input = Scaler(calc_mean=True, calc_std=False, axis=1).transform(matrix).round(4)
+
+        result = np.array([[0.5933, -.8067, .2133],
+                           [0.9, 3.3, -4.2],
+                           [0.9067, -.4533, -.4533]])
+
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                self.assertAlmostEqual(input[i, j], result[i][j])
+
+    # TODO: tests with calc_std=True
