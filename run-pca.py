710f5d8a5f9062db545a1684539f8712a7b7c99f
Mateusz Jamrowski <kermaxpl@gmail.com>
2019-04-22 19:49:50 +0200

    added default input options

--- run-pca.py
+++ run-pca.py
@@ -11,16 +11,15 @@ no_answers = ['N', 'n']
 
 answers = yes_answers + no_answers
 
-file_name = input("Proszę podać ścieżkę do pliku")
+file_name = input("Proszę podać ścieżkę do pliku: [data.xlsx] ").strip() or "data.xlsx"
 
-index_col = input("Podaj nazwę kolumny z indeksami wierszy: [Protein IDs] ")
-index_col = index_col or "Protein IDs"
+index_col = input("Podaj nazwę kolumny z indeksami wierszy: [Protein IDs] ").strip() or "Protein IDs"
 
 # file_name = '/home/mateusz/Pobrane/iris.csv'
 
 matrix = data.Holder(file_name, index_col=index_col)
 
-print("Wybór nazw kolumn zawierających dane do analizy: ")
+print("Wybór nazw kolumn zawierających dane do analizy:")
 print("\t 1. Intensity")
 print("\t 2. Concentration")
 print("\t 3. Własne wyrażenie regularne.")
@@ -29,31 +28,31 @@ filters = {"1": "^Intensity",
            "2": "^Concentration",
            "3": None}
 
-choice = input("Twórj wybór: ")
+choice = input("Twórj wybór: [1] ").strip() or "1"
 
 while choice not in filters.keys():
-    choice = input("Twój wybór: ")
+    choice = input("Twórj wybór: [1] ").strip() or "1"
 
 if choice == "3":
-    filters[choice] = input("Podaj wyrażenie regularne:").strip()
+    filters[choice] = input("Podaj wyrażenie regularne: [.*]").strip() or ".*"
 
 matrix.data = matrix.data.filter(regex=filters[choice])
 
-center_data = input("Czy przeprowadzić centrowanie danych względem średniej? [T/n]").strip()
+center_data = input("Czy przeprowadzić centrowanie danych względem średniej? [T/n]").strip() or "T"
 
 while center_data not in answers:
-    center_data = input("Czy przeprowadzić centrowanie danych względem średniej? [T/n]").strip()
+    center_data = input("Czy przeprowadzić centrowanie danych względem średniej? [T/n]").strip() or "T"
 
-standarize_data = input("Czy przeskalować dane tak, aby wariancja w kolumnach wnosiła 1?[T/n]").strip()
+standarize_data = input("Czy przeskalować dane tak, aby wariancja w kolumnach wnosiła 1? [T/n]").strip() or "T"
 while standarize_data not in answers:
-    standarize_data = input("Czy przeskalować dane tak, aby wariancja w kolumnach wnosiła 1?").strip()
+    standarize_data = input("Czy przeskalować dane tak, aby wariancja w kolumnach wnosiła 1? [T/n]").strip() or "T"
 
 print("Indeksy wymiaru 0: {}...".format(str(list(matrix.data.index.values))[:50]))  # TODO: do poprawy matrix.data
 print("Indeksy wymiaru 1: {}...".format(str(list(matrix.data.columns))[:50]))  # TODO: poprawy matrix.data
 
-dimm_to_compress = input("Który z wymiarów poddać kompresji?").strip()
+dimm_to_compress = input("Który z wymiarów poddać kompresji? [1] ").strip() or "1"
 while dimm_to_compress not in ["0", "1"]:
-    dimm_to_compress = input("Który z wymiarów poddać kompresji?").strip()
+    dimm_to_compress = input("Który z wymiarów poddać kompresji? [1] ").strip() or "1"
 dimm_to_compress = int(dimm_to_compress)
 
 valid_algorithms = {
@@ -66,9 +65,9 @@ print("Dostępne algorytmy dekompozycji:")
 for alg in valid_algorithms.keys():
     print('\t{}'.format(alg))
 
-algorithm = input("Proszę wybrać algorytm:").strip()
+algorithm = input("Proszę wybrać algorytm: [eig] ").strip() or "eig"
 while algorithm not in valid_algorithms.keys():
-    algorithm = input("Proszę wybrać algorytm: ").strip()
+    algorithm = input("Proszę wybrać algorytm: [eig] ").strip() or "eig"
 
 center_data = True if center_data in yes_answers else False
 standarize_data = True if standarize_data in yes_answers else False  # TODO: rename
@@ -91,7 +90,8 @@ elif algorithm == 'qrsvd':
     PCA.fit(standarized_matrix)
 
 print("Ilość wyjaśnianej wariancji przez kolejne zmienne {}".format(PCA.explained_ratio))
-n_components = input("Ilu głównych składowych użyć do rzutowania?")  # TODO: co to ma byc?
+n_components = input("Ilu głównych składowych użyć do rzutowania? [{}]".format(len(PCA.components))) \
+               or str(len(PCA.components))  # TODO: co to ma byc?
 PCA.number_of_components = int(n_components)
 transformed = PCA.transform(standarized_matrix)
 
@@ -99,4 +99,4 @@ result = pd.DataFrame(transformed,
                       index=matrix.data.index if dimm_to_compress else matrix.data.columns,
                       columns=["".join(("PC", str(i + 1))) for i in range(int(n_components))])
 
-result.to_excel("result.xlsx")
+result.to_excel("result.xlsx")  # TODO: definiowanie nazwy pliku wyjściowego
