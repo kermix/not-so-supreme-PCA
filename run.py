da80b5d36130259f268feeac1831496e3b16af43
Mateusz Jamrowski <kermaxpl@gmail.com>
2019-04-29 13:18:00 +0200

    Added own webview to minimize chance of unpredicted scenarios.

--- run.py
+++ run.py
@@ -1,4 +1,36 @@
+import sys
+import threading
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+
 from webgui.home import app
 
+
+def run_app(app_port=8050):
+    app.run_server(port=app_port,
+                   debug=False)
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True, threaded=False, processes=1)
+    port = 8050
+
+    threading.Thread(target=run_app, args=(port,), daemon=False).start()
+
+
+    def _downloadRequested(item):  # QWebEngineDownloadItem
+        print('downloading file to:', item.path())
+        item.accept()
+
+
+    qtapp = QApplication(sys.argv)
+
+    web = QWebEngineView()
+    web.setContextMenuPolicy(Qt.PreventContextMenu)
+    web.page().profile().downloadRequested.connect(_downloadRequested)
+    web.load(QUrl("http://127.0.0.1:8050"))
+    web.show()
+
+    sys.exit(qtapp.exec_())
