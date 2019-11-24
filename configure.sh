a5cacc209f3fb1f50b8db3626e053fd6e094dd5e
Mateusz Jamrowski <kermaxpl@gmail.com>
2019-11-24 13:09:27 +0100

    fix for vars with spaces

--- configure.sh
+++ configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-appdir="$(dirname $(readlink -f $BASH_SOURCE))"
+appdir="$(dirname $(readlink -f "$BASH_SOURCE"))"
 
 deactivate
 
 pip3 install -U --user virtualenv
 
-python3 -m virtualenv --python=python3 --no-site-packages $appdir
+python3 -m virtualenv --python=python3 --no-site-packages "$appdir"
 
-source $appdir/bin/activate; pip3 install -U -r $appdir/requirements.txt
+source "$appdir"/bin/activate; pip3 install -U -r "$appdir"/requirements.txt
