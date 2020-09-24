ee403609d73d82e4740c6d05de6500380b11e01d
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-09-24 14:28:53 +0200

    Fixed looking for script directory and creatind virtualenv

--- configure.sh
+++ configure.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-appdir="$(dirname $(readlink -f $BASH_SOURCE))"
+# @source https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 deactivate
 
 pip3 install -U --user virtualenv
 
-python3 -m virtualenv --python=python3 --no-site-packages $appdir
+python3 -m virtualenv --python=python3 $DIR
 
-source $appdir/bin/activate; pip3 install -U -r $appdir/requirements.txt
+source $DIR/bin/activate; pip3 install -U -r $DIR/requirements.txt
